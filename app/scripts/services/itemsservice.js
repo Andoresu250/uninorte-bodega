57f6f0406f7ed1c6d5581454d9f7d052d213b157
Andres Felipe Vera Arenas <andres-vera95@hotmail.com>
2016-09-21 09:49:17 -0500

    add items services and edit event

--- app/scripts/services/itemsservice.js
+++ app/scripts/services/itemsservice.js
@@ -1,23 +1,54 @@
 'use strict';
 
-/**
- * @ngdoc service
- * @name bodegaUninorteApp.itemsService
- * @description
- * # itemsService
- * Factory in the bodegaUninorteApp.
- */
 angular.module('bodegaUninorteApp')
-  .factory('itemsService', function () {
-    // Service logic
-    // ...
-
-    var meaningOfLife = 42;
-
-    // Public API here
+  .factory('itemsService', function ($http, sessionService, urlConstant) {
     return {
-      someMethod: function () {
-        return meaningOfLife;
+      new: function (itemData) {
+        return $http({
+          method: 'POST',
+          url: urlConstant + 'items/',
+          data: { name: itemData.name,
+                  item_type_id: itemData.item_type_id,
+                  number: itemData.numer,
+                  price: itemData.price,
+                  reorder: itemData.reorder,
+                  min_stock: itemData.min_stock },
+          headers:{ 'Authorization': sessionService.get('token') }
+        });
+      },
+      all: function () {
+        return $http({
+          method: 'GET',
+          url: urlConstant + 'items/',
+          headers:{ 'Authorization': sessionService.get('token') }
+        });
+      },
+      get: function (id) {
+        return $http({
+          method: 'GET',
+          url: urlConstant + 'items/' + id,          
+          headers:{ 'Authorization': sessionService.get('token') }
+        });
+      },
+      edit: function (itemData) {
+        return $http({
+          method: 'PUT',
+          url: urlConstant + 'items/' + itemData.id,        
+          data: { name: itemData.name,
+                  item_type_id: itemData.item_type_id,
+                  number: itemData.numer,
+                  price: itemData.price,
+                  reorder: itemData.reorder,
+                  min_stock: itemData.min_stock },
+          headers:{ 'Authorization': sessionService.get('token') }
+        });
+      },
+      delete: function (id) {
+        return $http({
+          method: 'DELETE',
+          url: urlConstant + 'items/' + id,          
+          headers:{ 'Authorization': sessionService.get('token') }
+        });
       }
-    };
+    };  
   });
